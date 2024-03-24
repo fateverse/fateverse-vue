b2eb70be1434d2403f1466205981567e3a204fd0
lilinyuan <1084668738@qq.com>
2024-03-24 23:13:56 +0800

    feat: 新增路由参数

--- src/stores/permisstion.js
+++ src/stores/permisstion.js
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import { defineAsyncComponent, ref } from "vue";
+import { ref } from "vue";
 import { getRouters } from "@/api/system/menu";
 import Layout from '@/layout/index.vue'
 import ParentView from '@/components/ParentView.vue'
@@ -43,14 +43,6 @@ export const usePermisstionStroe = defineStore('permisstion', () => {
           route.component = loadView(route.component)
         }
       }
-      if (route.pathParams != null) {
-        let pathParams = route.pathParams
-        route.pathParams = JSON.parse(route.pathParams)
-        route.query = {}
-        for (const key in route.pathParams) {
-          route.query = {[key]: route.pathParams[key]}
-        }
-      }
       if (route.children !== null && route.children && route.children.length !== 0) {
         route.children = formatAsyncRouters(route.children)
       }
@@ -69,10 +61,30 @@ export const usePermisstionStroe = defineStore('permisstion', () => {
       if (item.children && item.children.length !== 0) {
         generateMenu(item.children)
       }
+      if (item.pathParams != null) {
+        item.path = formatPath(item.path, JSON.parse(item.pathParams))
+      }
       item.title = item.meta.title
       item.icon = item.meta.icon
       return true
     })
+  }
+
+  // 拼接地址
+  const formatPath = (path, query) =>  {
+    const queryArr = []
+    let newPath = path
+    for (const key in query) {
+      queryArr.push({label: key, value: query[key]})
+    }
+    queryArr.forEach((item, index)=>{
+      if(!index) {
+        newPath += `?${item.label}=${item.value}`
+      } else {
+        newPath += `&${item.label}=${item.value}`
+      }
+    })
+    return newPath
   }
 
 
@@ -84,12 +96,18 @@ export const usePermisstionStroe = defineStore('permisstion', () => {
     }
   }
 
+  const removeMenu = () => {
+    menuList.value.length = 0
+    asyncRouters.value.length = 0
+  }
+
 
   return {
     asyncRouters,
     menuList,
     isLoadRoutes,
     setAsyncRouters,
-    setIsLoadRoutes
+    setIsLoadRoutes,
+    removeMenu
   }
 })
